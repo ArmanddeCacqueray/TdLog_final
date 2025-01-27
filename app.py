e57d70453341e4f9d176b033698df039baeba332
ArmanddeCacqueray <armanddecacqueray@sfr.fr>
2025-01-27 02:29:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import secrets
 import os
 import fitz  # PyMuPDF
 import yt_dlp   
-import main
+import utils
 import subprocess
 import whisper
 from datetime import datetime
@@ -22,7 +22,7 @@ app = Flask(__name__)
 app.secret_key = secret_key
 
 # Configuration de la base de données
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_new_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ddatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -54,17 +54,10 @@ class User(db.Model):
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     video_url = db.Column(db.String(255), nullable=False)
-    video_length = db.Column(db.Integer, nullable=True)  # Longueur de la vidéo en secondes
+    video_length = db.Column(db.Integer, nullable=False)  # Longueur de la vidéo en secondes
     transcription = db.Column(db.Text, nullable=True) 
     compterendu = db.Column(db.Text, nullable=True) 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __init__(self, video_url, video_length=None, user_id, transcription=None, comptrendu=None):
-            self.video_url = video_url
-            self.video_length = video_length
-            self.user_id = user_id
-            self.transcription = transcription
-            self.compterendu = compterendu
 
 
 # Créer les tables dans la base de données
@@ -104,7 +97,9 @@ def ajouter_transcription():
         try:
             # Créer un nouvel enregistrement avec juste du texte (transcription)
             new_video = Video(
-                video_url=video_name,  # Laisser vide car ce n'est pas une vidéo
+                video_url=video_name, 
+                video_length=0,
+                compterendu=None,
                 user_id=user_id,
                 transcription=transcription_text
             )
@@ -133,6 +128,7 @@ def ajouter_video():
     if request.method == 'POST':
         # Récupérer l'URL ou le fichier de la vidéo
         video_url = request.form.get('video_url')
+        videolength = request.form.get('video_length')
         if video_url:
             try:
                 print(f"Téléchargement de la vidéo depuis l'URL : {video_url}")
@@ -161,7 +157,7 @@ def ajouter_video():
         try:
             # Transcrire la vidéo avec Whisper
             print(f"Chargement du modèle Whisper...")
-            transcription =  main.transcribe_video(video_path, save=False)
+            transcription =  utils.transcribe_video(video_path, save=False)
 
             # Vérifier la transcription
             print(f"Transcription de la vidéo : {transcription[:100]}...")  # Afficher les 100 premiers caractères de la transcription
@@ -169,9 +165,10 @@ def ajouter_video():
             # Créer un nouvel enregistrement de vidéo avec la transcription
             new_video = Video(
                 video_url=video_path,
-                video_length=len(transcription.split()),  # Longueur basée sur le nombre de mots
+                video_length=videolength,  # Longueur basée sur le nombre de mots
                 user_id=user_id,
-                transcription=transcription
+                transcription=transcription,
+                compterendu=None
             )
             print(f"Ajout de la vidéo à la base de données : {new_video.video_url}, longueur : {new_video.video_length} mots")
             db.session.add(new_video)
@@ -370,17 +367,17 @@ def before_request():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     user_id = session.get('user_id')
-    mon_resume=main.compterendu()
-    reunion = Reunion.query.get(reunion_id)
-    reunion.compterendu = mon_resume
+    mon_resume=utils.compterendu()
+    video = Video.query.get(user_id)
+    video.compterendu = mon_resume
     db.session.commit()
     return render_template("upload.html", dynamic_message=Markup(mon_resume))
 
 
 # Fonction pour effectuer la complétion GPT-3.5
 filename = os.path.join(os.path.dirname(__file__), "transcription.txt")
-document = main.read_txt(filename)
-chunks = main.split_text(document)  
+document = utils.read_txt(filename)
+chunks = utils.split_text(document)  
 message_history = []
 def gpt3_completion(prompt_user, model="gpt-3.5-turbo", max_tokens=450):
     global message_history
@@ -465,12 +462,12 @@ def compterendu_anime():
     transcription_path = os.path.join(os.path.dirname(__file__), "transcription.txt")
     
     # Lecture des fichiers
-    model_text = main.read_txt(model_path)
-    transcription_text = main.read_txt(transcription_path)
+    model_text = utils.read_txt(model_path)
+    transcription_text = utils.read_txt(transcription_path)
     
     # Découper le texte en morceaux
-    model_chunks = main.split_text(model_text)
-    transcription_chunks = main.split_text(transcription_text)
+    model_chunks = utils.split_text(model_text)
+    transcription_chunks = utils.split_text(transcription_text)
 
     # Ajouter les morceaux dans l'historique des messages
     message_history.clear()
@@ -527,12 +524,12 @@ def compterendu_basededonnee():
     transcription_path = os.path.join(os.path.dirname(__file__), "transcription.txt")
     
     # Lecture des fichiers
-    model_text = main.read_txt(model_path)
-    transcription_text = main.read_txt(transcription_path)
+    model_text = utils.read_txt(model_path)
+    transcription_text = utils.read_txt(transcription_path)
 
     # Découper le texte en morceaux
-    model_chunks = main.split_text(model_text)
-    transcription_chunks = main.split_text(transcription_text)
+    model_chunks = utils.split_text(model_text)
+    transcription_chunks = utils.split_text(transcription_text)
 
     # Ajouter les morceaux dans l'historique des messages
     message_history.clear()
@@ -578,8 +575,7 @@ def basededonnee():
         result=compterendu_basededonnee()
         return render_template("upload_safe.html", dynamic_message=format_text_for_html(Markup(result)))
 
-# Exécution de l'application Flask
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+
